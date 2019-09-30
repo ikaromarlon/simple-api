546a7d30e6414d8ce5f5c36cf71a2648a49a2839
Icaro Silva <icaro.silva@webjump.com.br>
2019-09-30 14:59:21 -0300

    incresing User model test coverage

--- tests/unit/user.test.js
+++ tests/unit/user.test.js
@@ -1,4 +1,6 @@
 const uuidValidate = require('uuid-validate');
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 const UserFactory = require('../factories/UserFactory');
 
 describe('User', () => {
@@ -6,6 +8,16 @@ describe('User', () => {
   it('should create a user with a valid uuid', () => {
     const user = UserFactory();
     expect(uuidValidate(user._id)).toBe(true);
+  });
+
+  it('should generate a valid jwt token', async () => {
+    const user = UserFactory();
+
+    await user.generateToken();
+
+    const result = await promisify(jwt.verify)(user.token, 'gJ6JlxgWKj21swOsv7vm5Q');
+
+    expect(result.sub).toBe(user._id);
   });
 
 });
